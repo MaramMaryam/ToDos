c9968859bcfe5394a41db48738f92ba6783dcdff
Maryam <dev.m.danehvash2014@gmail.com>
2020-05-18 11:27:17 +0430

    create a header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Nav from "./components/Nav";
 
 function App() {
     return (
         <>
-            <h2> ToDo App </h2>
+           <Nav/>
         </>
     );
 
