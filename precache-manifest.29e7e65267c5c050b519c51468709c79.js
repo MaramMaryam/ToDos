b4cdfcdee2096380366cfd11939308377081f9b5
Maryam <dev.m.danehvash2014@gmail.com>
2020-05-19 22:00:56 +0430

    Updates

---
+++ precache-manifest.29e7e65267c5c050b519c51468709c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b0c019fb3ce9d84d94207faedab6bf",
+    "url": "/ToDos/index.html"
+  },
+  {
+    "revision": "8d92605adc411ddf1047",
+    "url": "/ToDos/static/css/main.e25c3694.chunk.css"
+  },
+  {
+    "revision": "e70ffa62d0928bf837ab",
+    "url": "/ToDos/static/js/2.14617d61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDos/static/js/2.14617d61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d92605adc411ddf1047",
+    "url": "/ToDos/static/js/main.ff511564.chunk.js"
+  },
+  {
+    "revision": "0d8230a90090c72bf27c",
+    "url": "/ToDos/static/js/runtime-main.ba79e7c8.js"
+  }
+]);
